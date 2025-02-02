95884277ff74d06e7e97614e4a1234b94a0dde60
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-02-02 12:03:59 -0800

    backend

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose');
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/test')
+  .then(() => console.log('Connected!'));
+
+
+app.use(cors())
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
