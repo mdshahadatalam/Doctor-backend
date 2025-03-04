6cd37dbbff714fd83471135ec611685c2fb09b65
mdshahadatalamm <mdshahadatalam25@gmail.com>
2025-03-04 11:20:12 -0800

    get

--- index.js
+++ index.js
@@ -125,6 +125,11 @@ app.get('/addDocItem',async function(req,res){
   
 })
 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
